48a323e70d5b4a70443384f75de5aabbafc8d2f6
“Shuyi_00550334” <shu.yi.chiangg@gmail.com>
2024-08-21 08:53:00 +0800

    8/21

---
+++ JavaScript/JavaScript評量/HashMap.js
@@ -0,0 +1,29 @@
+var HashMap = function () {
+  let map = {};
+  return {
+    // 向HashMap中添加一個key:value
+    put: function (key, value) {
+      map[key] = value;
+    },
+
+    // 獲取HashMap中所有的key
+    keys: function () {
+      return Object.keys(map);
+    },
+
+    // 檢查HashMap中是否包含某個key
+    contains: function (key) {
+      return map.hasOwnProperty(key);
+    },
+
+    // 根據key獲取對應的value
+    get: function (key) {
+      return map[key];
+    },
+
+    // 清空HashMap
+    clear: function () {
+      map = {};
+    },
+  };
+};
